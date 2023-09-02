ee1dadb191b0de4a00eb2f35553afefd7909bc46
Nickiiz <s6319c10004@sau.ac.th>
2023-09-02 14:55:21 +0700

    2 commit...

---
+++ lib/views/show_shop_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ShowShopUI extends StatefulWidget {
+  const ShowShopUI({super.key});
+
+  @override
+  State<ShowShopUI> createState() => _ShowShopUIState();
+}
+
+class _ShowShopUIState extends State<ShowShopUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink[100],
+    );
+  }
+}
