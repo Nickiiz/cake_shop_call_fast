60933b8a8cd46b6dabb11baa2b2ab13186437e77
Nickiiz <s6319c10004@sau.ac.th>
2023-09-12 08:29:00 +0700

    yaml and Splash_screen

--- lib/views/splash_screen_ui.dart
+++ lib/views/splash_screen_ui.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreenUI extends StatefulWidget {
   const SplashScreenUI({super.key});
@@ -14,12 +15,81 @@ class _SplashScreenUIState extends State<SplashScreenUI> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.pink[100],
-      appBar: AppBar(
-        backgroundColor: Colors.pink,
-        title: Text(
-          'CAKE SHOP CALL FAST',
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg_welcome.png'),
+            fit: BoxFit.cover,
+          ),
         ),
-        centerTitle: true,
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.2,
+              ),
+              Text(
+                'CAKE SHOP CALL FAST',
+                style: GoogleFonts.kanit(
+                  color: Colors.pink,
+                  fontWeight: FontWeight.bold,
+                  fontSize: MediaQuery.of(context).size.width * 0.07,
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.01,
+              ),
+              Text(
+                'สายด่วนชวนกินเค้ก',
+                style: GoogleFonts.kanit(
+                  color: Colors.pink,
+                  fontWeight: FontWeight.bold,
+                  fontSize: MediaQuery.of(context).size.width * 0.05,
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.01,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width * 0.1,
+                  right: MediaQuery.of(context).size.width * 0.1,
+                ),
+                child: Text(
+                  'Cake shops in Thailand that deliver delicious and gorgeously decorated birthday cakes for your special day.',
+                  style: GoogleFonts.kanit(
+                    color: Colors.grey,
+                    fontSize: MediaQuery.of(context).size.width * 0.04,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.02,
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.cake,
+                ),
+                label: Text(
+                  "let's go! สั่งกันเลย",
+                  style: GoogleFonts.kanit(),
+                ),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(
+                    MediaQuery.of(context).size.width * 0.5,
+                    MediaQuery.of(context).size.height * 0.015,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  backgroundColor: Colors.pink,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
